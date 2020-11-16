b525860dc0fe4a0f12875cf02964bc5edd8e77ca
robinleruth <robin.leruth@gmail.com>
2020-11-16 15:06:22 +0100

    Change controller name for users

--- users/app/interface/__init__.py
+++ users/app/interface/__init__.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-api = FastAPI(title='Token API',
+api = FastAPI(title='Users API',
               description='',
               version='0.1')
 
 from .controllers import user_controller
 
 api.include_router(user_controller.router,
-                   prefix='/api/v1/token_controller',
-                   tags=['token_controller'])
+                   prefix='/api/v1/user_controller',
+                   tags=['user_controller'])
 
 api.add_middleware(
     CORSMiddleware,

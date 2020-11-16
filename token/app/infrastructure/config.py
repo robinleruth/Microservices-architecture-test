f37db8b7bf7a727d9ec1ae28f9b51a9200848eaf
robinleruth <robin.leruth@gmail.com>
2020-11-16 15:24:06 +0100

    config

--- token/app/infrastructure/config.py
+++ token/app/infrastructure/config.py
@@ -1,2 +1,26 @@
+import os
+
+assert 'APP_ENV' in os.environ, 'MAKE SURE TO SET AN ENVIRONMENT'
+
+
 class Config:
+    PORT = 8082
+
+
+class DockerConfig(Config):
+    PORT = 8080
+
+
+class TestConfig(Config):
     pass
+
+
+env = os.environ['APP_ENV'].upper()
+if env == 'TEST':
+    app_config = TestConfig
+elif env == 'PRD':
+    app_config = DockerConfig
+else:
+    app_config = Config
+
+print(app_config)

a0db0688b5d16464e24743d9180e62dcbf68e38a
robinleruth <robin.leruth@gmail.com>
2020-11-17 19:35:38 +0100

    Update db field type

--- users/app/domain/model/user.py
+++ users/app/domain/model/user.py
@@ -1,11 +1,12 @@
+import datetime as dt
 from dataclasses import dataclass
 
 
 @dataclass
 class User:
     id: int
-    created_at: int
-    updated_at: int
-    last_seen_at: int
+    created_at: dt.datetime
+    updated_at: dt.datetime
+    last_seen_at: dt.datetime
     nickname: str
     online: bool
--- users/app/infrastructure/db/db_user.py
+++ users/app/infrastructure/db/db_user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, func, String, Boolean
+from sqlalchemy import Column, func, String, Boolean, DateTime
 from sqlalchemy import Integer
 
 from app.domain.services import password_service
@@ -9,9 +9,9 @@ class DbUser(Base):
     __tablename__ = 'User'
 
     id = Column(Integer, primary_key=True)
-    created_at = Column(Integer, default=func.now())
-    updated_at = Column(Integer, default=func.now(), onupdate=func.now())
-    last_seen_at = Column(Integer, default=func.now())
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    last_seen_at = Column(DateTime, default=func.now())
     nickname = Column(String(32), nullable=False, unique=True)
     password_hash = Column(String(256), nullable=False)
     online = Column(Boolean, default=False)

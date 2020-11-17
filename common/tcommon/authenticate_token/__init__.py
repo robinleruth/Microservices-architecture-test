6a810d50f965bd03f7be5c9f626a2dd62565f96f
robinleruth <robin.leruth@gmail.com>
2020-11-17 11:10:43 +0100

    Common package set up, authenticate token function

---
+++ common/tcommon/authenticate_token/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+import requests
+
+from tcommon.authenticate_token.unauthorized_exception import UnauthorizedException
+from tcommon.config import app_config
+
+logger = logging.getLogger(__name__)
+
+
+def get_user_info_by_token(token: str) -> {}:
+    url = app_config.TOKEN_SERVICE_URL + app_config.TOKEN_INFO
+    logger.info(f"GET {url}")
+    r = requests.get(url, headers={"Authorization": f"Bearer {token}"})
+    if r.status_code != 200:
+        if r.status_code == 401:
+            raise UnauthorizedException(str(r.json()))
+        else:
+            raise Exception(f'An error occured while getting {url} : status code {r.status_code}, message {r.json()}')
+    return r.json()
---
+++ common/tcommon/config.py
@@ -0,0 +1,28 @@
+import os
+
+assert 'APP_ENV' in os.environ, 'MAKE SURE TO SET AN ENVIRONMENT'
+
+
+class Config:
+    TOKEN_SERVICE_URL = 'http://localhost:8082/api/v1/token_controller/'
+    TOKEN_CREATION = 'token'
+    TOKEN_INFO = 'tokenInfo'
+
+
+class DockerConfig(Config):
+    TOKEN_SERVICE_URL = 'http://token:8080/api/v1/token_controller/'
+
+
+class TestConfig(Config):
+    pass
+
+
+env = os.environ['APP_ENV'].upper()
+if env == 'TEST':
+    app_config = TestConfig
+elif env == 'PRD':
+    app_config = DockerConfig
+else:
+    app_config = Config
+
+print(app_config)

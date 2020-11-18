7bd03f6c8a5210b227fb0f32e0aa3eb7cf31cf5c
robinleruth <robin.leruth@gmail.com>
2020-11-18 10:40:51 +0100

    memoize token for expire_in

--- common/tcommon/auth_request/token_auth.py
+++ common/tcommon/auth_request/token_auth.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import logging
 from typing import List
 
@@ -9,16 +10,34 @@ from tcommon.config import app_config
 logger = logging.getLogger(__name__)
 
 
+def memoize_token(func):
+    cached_token = None
+
+    def inner(self, *args, **kwargs):
+        nonlocal cached_token
+        if self.expire_time is not None:
+            remaining = self.expire_time - dt.datetime.utcnow()
+            print(f'Remaining : {remaining}')
+        if cached_token is None or (self.expire_time is not None and remaining.seconds < 10):
+            print('No token cached')
+            cached_token = func(self, *args, **kwargs)
+        return cached_token
+
+    return inner
+
+
 class TokenAuth(AuthBase):
     def __init__(self, username: str, password: str, scopes: List[str]):
         self.username = username
         self.password = password
         self.scopes = ' '.join(scopes)
+        self.expire_time = None
 
     def __call__(self, r):
         r.headers['Authorization'] = f"Authorization {self._get_token()}"
         return r
 
+    @memoize_token
     def _get_token(self):
         headers = {
             "Content-Type": "application/x-www-form-urlencoded",
@@ -30,4 +49,5 @@ class TokenAuth(AuthBase):
         r = requests.post(url, headers=headers, data=data)
         token = r.json()['access_token']
         expire_in = r.json()['expire_in']
+        self.expire_time = dt.datetime.utcnow() + dt.timedelta(minutes=expire_in)
         return token
--- common/tests/test_token_auth.py
+++ common/tests/test_token_auth.py
@@ -11,11 +11,15 @@ from tcommon.auth_request.token_auth import TokenAuth
 
 class TestTokenAuth(unittest.TestCase):
     def test_token_auth(self):
-        s = Session()
-        s.auth = TokenAuth(username='Robin', password='test', scopes=['me'])
-        with patch('requests.post') as mock_request:
-            ret = MagicMock()
-            ret.json = lambda: {'access_token': '123', 'expire_in': 1}
-            mock_request.return_value = ret
-            r = s.get('http://localhost:8082/api/v1/token_controller/tokenInfo')
-            print(r)
+        with patch.object(Session, 'send', return_value='ok') as mock_method:
+            s = Session()
+            s.auth = TokenAuth(username='Robin', password='test', scopes=['me'])
+            with patch('requests.post') as mock_request:
+                ret = MagicMock()
+                ret.json = lambda: {'access_token': '123', 'expire_in': 3}
+                mock_request.return_value = ret
+                r = s.get('http://localhost:8082/api/v1/token_controller/tokenInfo')
+                print(r)
+                r = s.get('http://localhost:8082/api/v1/token_controller/tokenInfo')
+                print(r)
+        mock_request.assert_called_once()
